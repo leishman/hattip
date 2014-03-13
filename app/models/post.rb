3ea101b49fde637b9b6106cae73e5a6ffdedc25e
Alexander Leishman <leishman3@gmail.com>
2014-03-13 00:16:46 -0700

    moved youtube_buddy.rb to proper folder. Added video thumbnails to front page

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ActiveRecord::Base
 	belongs_to :user
 	validates :youtube_url, presence: true
 	validates :title, presence: true, length: 4..50
+
+	def thumbnail_url
+		YoutubeBuddy.new(youtube_url).thumbnail_url
+	end
 end
