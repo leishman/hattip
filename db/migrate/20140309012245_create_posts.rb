4d66586a66a582ffa1f1d0eda2e80c1700fb1a80
Alexander Leishman <leishman3@gmail.com>
2014-03-20 21:50:28 -0700

    updated migration

--- db/migrate/20140309012245_create_posts.rb
+++ db/migrate/20140309012245_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
     	t.belongs_to 	:user
-      
+    
     	t.string			:title
     	t.text				:description
     	t.string			:youtube_url
